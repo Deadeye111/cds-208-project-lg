9292c312d45dc74ee3c250f31ac175aabf409015
Deadeye111 <leandro01@bluewin.ch>
2024-05-16 16:35:15 +0200

    design updates

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -130,36 +130,34 @@ const updateBoard = async (boardID, boardName) => {
     <div class="container mx-auto pt-5 mt-5 pl-5 pr-5">
       <div class="flex justify-center">
         <div class="grid grid-cols-1 gap-5 w-full">
-          <div class="bg-white dark:bg-[#121212] p-5 shadow-md rounded-lg flex-grow border-2 border-solid border-slate-200">
-            <h1 class="text-xl font-bold text-center w-full h-auto mb-4 dark:text-white">
-              Welcome to <span v-if="username" class="bg-gradient-to-r from-[#00c6ff] to-[#0072ff] bg-clip-text text-transparent font-semibold drop-shadow">{{ username }}'s</span> Canban Dashboard
-            </h1>
+          <h1 class="text-xl font-bold text-center w-full h-auto mb-4 dark:text-white">
+            Welcome to <span v-if="username" class="bg-gradient-to-r from-[#00c6ff] to-[#0072ff] bg-clip-text text-transparent font-semibold drop-shadow">{{ username }}'s</span> Canban Dashboard
+          </h1>
 
-            <div class="flex flex-col items-center mt-5 pb-5 border-2">
-              <h1 class="dark:text-white text-lg font-bold w-full text-center mb-3 mt-3">Create Boards</h1>
-              <div class="flex items-center justify-center">
-                <input type="text" v-model="newBoardName" placeholder="Canban board Name.." class="px-4 py-2 sm:w-52 md:w-72 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
-                <button @click="createBoard" class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add</button>
-              </div>
+          <div class="flex flex-col items-center mt-5 pb-5 rounded-lg border-2 border-solid border-slate-200">
+            <h1 class="dark:text-white text-lg font-bold w-full text-center mb-3 mt-3">Create Boards</h1>
+            <div class="flex items-center justify-center">
+              <input type="text" v-model="newBoardName" placeholder="Canban board Name.." class="px-4 py-2 sm:w-52 md:w-72 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
+              <button @click="createBoard" class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add</button>
             </div>
+          </div>
 
-            <div class="flex flex-col items-center mt-10 border-2">
-              <h1 class="dark:text-white text-lg font-bold w-full text-center mb-3 mt-3">List of Boards</h1>
-              <ul>
-                <li v-for="(board, index) in boardArray" :key="index" class="dark:text-white mb-4">
-                  <div class="flex flex-col lg:flex-row lg:items-center lg:space-x-4">
-                    <div class="flex items-center mt-2 lg:mt-0">
-                      <span class="mr-2">Name:</span>
-                      <input @blur="updateBoard(board.id, board.name)" type="text" v-model="board.name" class="border border-gray-300 rounded-md px-2 py-1 text-black">
-                    </div>
-                    <div class="mt-2 lg:mt-0">Created at: {{ new Date(board.created_at).toLocaleString('de-CH', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }) }}</div>
-                    <NuxtLink :to="`/canbanBoard/${board.id}`" class="text-center bg-lime-500 text-white px-4 py-2 rounded-md hover:bg-lime-600 mt-2 lg:mt-0">View</NuxtLink>
-                    <button @click="deleteBoard(board.id)" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 mt-2 lg:mt-0">Delete</button>
-                    <hr class="mt-5" />
+          <div class="flex flex-col items-center mt-10 rounded-lg border-2 border-solid border-slate-200">
+            <h1 class="dark:text-white text-lg font-bold w-full text-center mb-3 mt-3">List of Boards</h1>
+            <ul>
+              <li v-for="(board, index) in boardArray" :key="index" class="dark:text-white mb-4">
+                <div class="flex flex-col lg:flex-row lg:items-center lg:space-x-4">
+                  <div class="flex items-center mt-2 lg:mt-0">
+                    <span class="mr-2">Name:</span>
+                    <input @blur="updateBoard(board.id, board.name)" type="text" v-model="board.name" class="border border-gray-300 rounded-md px-2 py-1 text-black">
                   </div>
-                </li>
-              </ul>
-            </div>
+                  <div class="mt-2 lg:mt-0">Created at: {{ new Date(board.created_at).toLocaleString('de-CH', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }) }}</div>
+                  <NuxtLink :to="`/canbanBoard/${board.id}`" class="text-center bg-lime-500 text-white px-4 py-2 rounded-md hover:bg-lime-600 mt-2 lg:mt-0">View</NuxtLink>
+                  <button @click="deleteBoard(board.id)" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 mt-2 lg:mt-0">Delete</button>
+                  <hr class="mt-5" />
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
