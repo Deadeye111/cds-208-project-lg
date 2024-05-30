728951f554c88edaeec8684bf1b28afe4f344992
Deadeye111 <leandro01@bluewin.ch>
2024-05-30 11:16:20 +0200

    testing

--- cypress/e2e/deployed_test.cy.js
+++ cypress/e2e/deployed_test.cy.js
@@ -13,14 +13,11 @@ describe('deployed test', () => {
     /* ==== Board Creation and updates ==== */
     cy.get(':nth-child(1) > .hover\\:underline').click();
     cy.get(':nth-child(1) > .inline-flex > .text-center').click();
-    cy.get('.sm\\:w-52').clear('tes');
     cy.get('.sm\\:w-52').type('testboard');
     cy.get('.ml-2').click();
     cy.get('.dark\\:text-white > .flex-col > .text-center').click();
-    cy.get(':nth-child(2) > .grid > .px-3').clear('tes');
     cy.get(':nth-child(2) > .grid > .px-3').type('test');
     cy.get(':nth-child(2) > .grid > .px-4').click();
-    cy.get(':nth-child(3) > .grid > input.py-2').clear('tes');
     cy.get(':nth-child(3) > .grid > input.py-2').type('test');
     cy.get(':nth-child(3) > .grid > .px-4').click();
     cy.get('.w-6').click();
